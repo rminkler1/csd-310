5f7d7442375adbb8247d1c6cd155c54987f89abf
robertminkler <robertminkler@gmail.com>
2024-09-13 22:16:24 -0700

    Add comments to python file.

--- module-6/mysql_test.py
+++ module-6/mysql_test.py
@@ -1,6 +1,13 @@
+# Robert Minkler
+# Sep 11, 2024
+# CSD 310 Module 6.2
+# Movies Setup py
+
+
 import mysql.connector
 from mysql.connector import errorcode
 
+# Configure db connection
 config = {
     "user": "movies_user",
     "password": "popcorn",
@@ -9,23 +16,32 @@ config = {
     "raise_on_warnings": True
 }
 
+
 try:
+    # connect to db
     db = mysql.connector.connect(**config)
 
     print("\n Database user {} connected to MySQL on host {}".format(config['user'],config['host'], config['database']))
 
-    input('\n\n Press any key to continue...')
+    # Wait for enter key press before ending the program
+    input('\n\n Press enter/return key to continue...')
 
 
 except mysql.connector.Error as err:
+    # print connection errors
+
+    # bad login
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("The supplied username or password are invalid")
 
+    # db not found
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
         print("The specified database does not exist")
 
+    # all other errors
     else:
         print(err)
 
+# close the db connection
 finally:
     db.close()
